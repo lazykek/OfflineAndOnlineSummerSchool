36733c60a758ff88e999d44d9651acab7317cdaf
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2026-06-13 17:19:29 +0300

    Initial Commit

---
+++ SummerSchoolProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SummerSchoolProject
+//
+//  Created by Ilia Cherkasov on 13.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
